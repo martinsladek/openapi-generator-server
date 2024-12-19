8cdb0e714f25a276a76f9bd8e76b4c300e247052
Martin Sladek <martinsladek@users.noreply.github.com>
2024-12-19 02:07:20 +0100

    OpenAPI generator - server-side stub - example project

---
+++ src/main/java/org/example/controller/PetsApiController.java
@@ -0,0 +1,25 @@
+import com.baeldung.openapi.api.PetsApi;
+import com.baeldung.openapi.api.PetsApiDelegate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen",
+        date = "2021-03-22T23:26:32.308871+05:30[Asia/Kolkata]")
+@Controller
+@RequestMapping("${openapi.swaggerPetstore.base-path:}")
+public class PetsApiController implements PetsApi {
+
+    private final PetsApiDelegate delegate;
+
+    public PetsApiController(
+            @org.springframework.beans.factory.annotation.Autowired(required = false) PetsApiDelegate delegate) {
+        this.delegate = Optional.ofNullable(delegate).orElse(new PetsApiDelegate() {});
+    }
+
+    @Override
+    public PetsApiDelegate getDelegate() {
+        return delegate;
+    }
+}
